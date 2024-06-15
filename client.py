f4e1060f25975fe15baf45ecb073f4ace8617fba
Lucas <114083832+Lucca7r@users.noreply.github.com>
2024-06-15 11:31:08 -0300

    img teste

--- client.py
+++ client.py
@@ -3,21 +3,29 @@ import websockets
 import base64
 from PIL import Image
 import io
+import time
 
-async def enviar_imagem(imagem_path):
-    url_servidor = 'wss://6a19d5da-37bf-40f9-93b5-ec6df8761273-00-2m8nm1n3blvla.picard.replit.dev:3000'
+async def enviar_imagens(imagens_paths):
+    inicio = time.time()
+    url_servidor = 'wss://a24ea60f-9742-49b2-b5f2-2e7c3e3ace54-00-2dgl6x615ioyh.picard.replit.dev:3000'
     
     async with websockets.connect(url_servidor) as websocket:
-        try:
-            with open(imagem_path, "rb") as imagem_original:
-                imagem64 = base64.b64encode(imagem_original.read()).decode()
-                await websocket.send(imagem64)
+        for index, imagem_path in enumerate(imagens_paths):
+            try:
+                with open(imagem_path, "rb") as imagem_original:
+                    imagem64 = base64.b64encode(imagem_original.read()).decode()
+                    await websocket.send(imagem64)
 
-                resultado = await websocket.recv()
-                imagem_resultado = base64.b64decode(str(resultado))
-                imagemB = Image.open(io.BytesIO(imagem_resultado))
-                imagemB.save("./out/NovaImagem.jpg")
-        except Exception as e:
-            print(f"Erro: {e}")
-            
-asyncio.run(enviar_imagem('./assets/galaxy.jpg'))
+                    resultado = await websocket.recv()
+                    imagem_resultado = base64.b64decode(str(resultado))
+                    imagemB = Image.open(io.BytesIO(imagem_resultado))
+                    # Garante que cada imagem salva tenha um nome único
+                    imagemB.save(f"./out/image_pb_{index}.jpg")
+            except Exception as e:
+                print(f"Erro ao enviar/receber a imagem {imagem_path}: {e}")
+    
+    print(f"Tempo total: {time.time() - inicio}")
+
+# Lista de caminhos das imagens a serem enviadas
+imagens_paths = ['./assets/galaxy.jpg', './assets/bigbang.jpg', './assets/tigre.jpg', './assets/unsplash (1).jpg', './assets/unsplash (2).jpg', './assets/unsplash (3).jpg', './assets/unsplash (4).jpg', './assets/unsplash (5).jpg']
+asyncio.run(enviar_imagens(imagens_paths))
